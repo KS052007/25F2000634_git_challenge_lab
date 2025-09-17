7499ce2b52b0c985a9c0dd231c6dfd4b30ed862b
Kartik S. <25f2000634@ds.study.iitm.ac.in>
2025-09-17 15:07:14 +0530

    Add intial calculator structure with main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+def calculator():
+    print(" Simple Calculator ")
+    print("This calculator will perform basic arithmetic operations")
+    print("Operations: +, -, *, /")
+
+if __name__ == "__main__":
+    calculator()
